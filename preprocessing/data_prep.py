2fead851ce2522819cc96952863c0bc139e1aafe
Anri <anryabrahamyan@mail.ru>
2022-02-07 23:11:55 +0400

    create main file for preprocessing

--- preprocessing/data_prep.py
+++ preprocessing/data_prep.py
@@ -1,6 +1,8 @@
 """
 File for preprocessing and augmenting data
 """
-
 import pandas as pd
 
+dataset = pd.read_csv("../datasets/tweet_emotions.csv").drop(['tweet_id'], inplace=True, axis=1)
+
+# TODO preprocessing for tweets
